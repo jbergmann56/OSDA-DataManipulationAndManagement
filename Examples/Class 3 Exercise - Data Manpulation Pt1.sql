a9cd5a61a9ea48b1b8edea14715ff532e826b7ed
jbergmann56 <jeremy@jbanalyticsconsulting.com>
2019-07-15 20:25:56 -0500

    change2

--- Examples/Class 3 Exercise - Data Manpulation Pt1.sql
+++ Examples/Class 3 Exercise - Data Manpulation Pt1.sql
@@ -1,13 +1,10 @@
--- 1. Create a schema named “MissingPersons”
+-- 1. Create a query using the “world.country” table that utilizes as many SQL operators as possible.  
 create schema MissingPersons; 
 use MissingPersons;
 
--- 2. Create a table in “MissingPersons” schema named “person”, with the following qualities: 
--- Fields:  ID, LastName, FirstName, Age
--- Primary Key: ID
--- Field Constraints: Not Nullable
+-- 2.Import a list of non-standard addresses, then use case statements to standardize them in a similar format.
  
-CREATE TABLE MissingPersons (
+CREATE TABLE Person (
   ID int not null, 
   LastName varchar(50), 
   FirstName varchar(50), 
@@ -15,29 +12,18 @@ CREATE TABLE MissingPersons (
   PRIMARY KEY (ID)
   );
 
--- 3. Alter the table to include (add) a “Gender” field.
-ALTER TABLE MissingPersons
+-- 3. Utilize the ifnull() function on a column to replace null values with the one of your choice. (must be same data type!)
+ALTER TABLE Person
 ADD Gender char(1);
 
--- 4.   Insert the following records into the classicmodels.customers table: 
-insert into MissingPersons (ID, LastName, FirstName, Age, Gender)
+-- 4.   In the “world” schema, obtain the name of all cities in North America that have a population over 1M, where the primary language is English.   
+insert into Person (ID, LastName, FirstName, Age, Gender)
 values 
 (1,'Doe','Jane',42,'F'),
 (2,'Doe','John',57,'M'),
 (3,'Presley','Elvis',82,'M'),
 (4,'Shakur','Tupac',49,'M')
 
--- 5. Delete the record with “LastName” = “Presley”
+-- 5. Using world.country table, combine all countries (repeating) with a life expectency of > 70 years or a GNP > 100000 
 -- if error SET SQL_SAFE_UPDATES = 0;
-Delete from  MissingPersons WHERE LastName = 'Presley';
-
--- 6. Update the LastName in the record that contains “ID” = 4 to “LastName” = ‘Crooks’
-update  MissingPersons
-SET LastName = 'Crooks'
-where ID = 4;
-
--- 7. drop the column "FirstName" from the table
-ALTER TABLE MissingPersons DROP FirstName;
-
--- 7. drop the "person" from the table
-Drop TABLE MissingPersons;
+Delete from Person WHERE LastName = 'Presley';
